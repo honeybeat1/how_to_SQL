f4377a00dc493758b7279595ab94d59d8485f8d0
honeybeat1 <honeybeat1@gmail.com>
2022-04-07 21:22:55 +0900

    fix: where phrase

--- hackerrank/weather_observation_station20.sql
+++ hackerrank/weather_observation_station20.sql
@@ -1,7 +1,6 @@
 -- median 구하기, set 별도 변수(칼럼) 만들기.
 set @rowindex = -1;
-select round(LAT_N,4) from (
+select round(AVG(LAT_N)),4) from (
 select @rowindex:=@rowindex+1 as row_index, LAT_N from station
 order by LAT_N)row_table
-where row_index in ((ceil(@rowindex/2) + floor(@rowindex/2))/2, 
-                    @rowindex/2)
+where row_index in (ceil(@rowindex/2), floor(@rowinde/2))
